4d3af083b449bae245441918363e2d21904be0e2
McCabe Northup <mccabenorthup@gmail.com>
2020-02-28 23:58:07 -0700

    landing page MVP

---
+++ client/src/pages/Weather/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const Weather = () => {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <h1>Weather Page</h1>
+            </Container>
+        </React.Fragment>
+    );
+};
+
+export default Weather;
